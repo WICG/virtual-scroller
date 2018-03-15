dc2ab40d9db4079f147a2812c3821396ddcdee8d
valdrinkoshi <valdrinkoshi@gmail.com>
2018-03-14 23:51:59 -0700

    default layout in config

--- flavors/lit-html/lit-list.js
+++ flavors/lit-html/lit-list.js
@@ -1,8 +1,9 @@
+import Layout from '../../layouts/layout-1d.js';
 import {VirtualList} from '../../virtual-list.js';
 import {LitMixin, repeat} from './lit-repeater.js';
 import {directive} from '/node_modules/lit-html/lit-html.js';
 
-const partToList = new WeakMap();
+const layout = new Layout({direction: 'vertical'});
 
 export const LitList = LitMixin(VirtualList);
 
@@ -10,6 +11,9 @@ export const list = (config) => {
   // Recycle by default.
   if (config && !config.hasOwnProperty('recycle')) {
     config.recycle = true;
+  }
+  if (config && !config.hasOwnProperty('layout')) {
+    config.layout = layout;
   }
   return repeat(config, LitList);
 };
