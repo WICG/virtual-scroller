35da494a47af03cff90792061c3487fb717d6e18
valdrinkoshi <valdrinkoshi@gmail.com>
2018-05-07 16:59:21 -0700

    no spaces in contenteditable elements

--- demo/contacts/lit-html/contacts.js
+++ demo/contacts/lit-html/contacts.js
@@ -1,12 +1,15 @@
 import {list} from '../../../lit-html/lit-list.js';
 import {html, render} from '../../../node_modules/lit-html/lib/lit-extended.js';
-import {itemType, Sample as BaseSample, strip} from '../contacts.js';
+import {itemType, Sample as BaseSample} from '../contacts.js';
 
 export class Sample extends BaseSample {
   _setUp() {
     this.template = (item, idx) => {
       const type = itemType(item);
       if (type === 'contact') {
+        // NOTE(valdrin): don't add spaces in `<p contenteditable>` so
+        // lit-html can place its delimiter comments
+        // https://github.com/Polymer/lit-html/issues/316
         return html`
                     <div
                         style="padding: 10px; border-bottom: 1px solid #CCC; width: 100%; box-sizing: border-box;"
@@ -17,10 +20,8 @@ export class Sample extends BaseSample {
                             contenteditable="true"
                             on-focus="${e => this._scrollToFocused(e)}"
                             on-blur="${
-            e => this._commitChange(idx, 'longText', e.target)}"
-                        >
-                            ${item.longText}
-                        </p>
+            e => this._commitChange(idx, 'longText', e.target.textContent)}"
+                        >${item.longText}</p>
                     </div>`;
       }
       return html`
@@ -28,17 +29,6 @@ export class Sample extends BaseSample {
                     ${item.title}
                 </div>`;
     };
-  }
-
-  _commitChange(idx, prop, el) {
-    if (idx === -1)
-      return;
-    // Restore old value, lit-html will handle the update
-    // https://github.com/Polymer/lit-html/issues/316
-    const newVal = strip(el.textContent);
-    const prevVal = strip(this.items[idx][prop]);
-    el.textContent = prevVal;
-    super._commitChange(idx, prop, newVal);
   }
 
   render() {
