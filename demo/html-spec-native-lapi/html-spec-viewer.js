11737156409b9213697f33dfeaabf08bea2e12af
Valdrin Koshi <valdrin@google.com>
2018-05-07 18:22:40 -0700

    use std:virtual-list for <html-spec-viewer> (#63)
    * add html-spec-viewer using native virtual-list, update demo page
    
    * remove examples.html reference
    
    * incorporate feedback

---
+++ demo/html-spec-native-lapi/html-spec-viewer.js
@@ -0,0 +1,73 @@
+import {VirtualListElement} from 'std:virtual-list';
+
+import {HtmlSpec} from '../../node_modules/streaming-spec/HtmlSpec.js';
+import {iterateStream} from '../../node_modules/streaming-spec/iterateStream.js';
+
+class HTMLSpecViewer extends VirtualListElement {
+  connectedCallback() {
+    super.connectedCallback();
+    if (!this._htmlSpec) {
+      const style = document.createElement('style');
+      style.textContent = `
+  :host {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    padding: 8px;
+    height: auto;
+  }`;
+      this.shadowRoot.appendChild(style);
+      if ('rootScroller' in document) {
+        document.rootScroller = this;
+      }
+
+      this._htmlSpec = new HtmlSpec();
+      this._htmlSpec.head.style.display = 'none';
+      this.appendChild(this._htmlSpec.head);
+
+      this.items = [];
+      this.addNextChunk();
+      this.addEventListener(
+          'rangechange', (event) => this.onRangechange(event));
+    }
+  }
+
+  newChild(item) {
+    return item;
+  }
+
+  recycleChild() {
+    // keep children in DOM.
+  }
+
+  async addNextChunk(chunk = 10) {
+    if (this._adding) {
+      return;
+    }
+    this._adding = true;
+    const stream = this._htmlSpec.advance(this.items[this.items.length - 1]);
+    for await (const el of iterateStream(stream)) {
+      if (/^(style|link|script)$/.test(el.localName)) {
+        this._htmlSpec.head.appendChild(el);
+      } else {
+        this.items.push(el);
+        chunk--;
+      }
+      if (chunk === 0) {
+        this.requestReset();
+        break;
+      }
+    }
+    this._adding = false;
+  }
+
+  onRangechange(range) {
+    if (range.last >= this.items.length - 4) {
+      this.addNextChunk();
+    }
+  }
+}
+
+customElements.define('html-spec-viewer', HTMLSpecViewer);
