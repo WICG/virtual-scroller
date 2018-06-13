36366e876dd39cb7b35bbce0715b32a8b087ce09
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2018-06-14 01:06:02 +0200

    Add demo: list with dismissable items (#96)
    * Add demo is a list with dismissable items
    
    * Fix review comments
    
    * Make the animation code slightly more readable
    
    * Fix issue that tracker might be null in _onTouchEnd
    
    * Use pointer events so you can dismiss using mouse as well
    
    * Only use pointer events for mouse
    
    Virtual scroller relies on regular touch events and in order to use
    pointer events for touch, you must set "touch-action: none" to block
    regular touch events, and regular touch events cancels touch based
    pointer events, so this is the only way.
    
    * Fix a few extra review comments
    
    * Get rid of preventDefault() and unneeded offsetWidth
    
    * Add cancel state to not be able to reenteract after cancelled
    
    * format
    
    * disable text selection
    
    * ignore events if scrolling vertically

---
+++ demo/dismissable/dismissable-item.js
@@ -0,0 +1,213 @@
+const kMinFlingVelocityValue = 0.4;
+const kTouchSlopValue = 5;
+
+class VelocityTracker {
+  constructor() {
+    this._recentTouchMoves = [];
+    this._timeWindow = 50;
+  }
+
+  _pruneHistory(timeStamp) {
+    const index = this._recentTouchMoves.findIndex((touch) => {
+      return touch.timeStamp > timeStamp - this._timeWindow;
+    });
+    this._recentTouchMoves.splice(0, index + 1);
+  }
+
+  update(e) {
+    this._pruneHistory(e.timeStamp);
+    this._recentTouchMoves.push(e);
+
+    const oldestTouchMove = this._recentTouchMoves[0];
+
+    const deltaX = e.clientX - oldestTouchMove.clientX;
+    const deltaT = e.timeStamp - oldestTouchMove.timeStamp;
+
+    return {velocityX: (deltaT > 0) ? deltaX / deltaT : 0};
+  }
+}
+
+class DismissableItem extends HTMLElement {
+  constructor() {
+    super();
+
+    this.position = 0;
+    this.itemIndex = 0;
+    this.width = 0;
+    this.state = 'initial';
+    this.addEventListener('touchstart', this);
+    this.addEventListener('touchmove', this);
+    this.addEventListener('touchend', this);
+    this.addEventListener('pointerdown', this);
+    this.addEventListener('pointermove', this);
+    this.addEventListener('pointerup', this);
+  }
+
+  handleEvent(event) {
+    if (event.pointerType && event.pointerType !== 'mouse') {
+      return;
+    }
+
+    switch (event.type) {
+      case 'pointerdown':
+        this.setPointerCapture(event.pointerId);
+        this._onPointerDown(event);
+        break;
+      case 'pointermove':
+        if (event.pressure) {
+          this._onPointerPan(event);
+        }
+        break;
+      case 'pointerup':
+        this.releasePointerCapture(event.pointerId);
+        this._onPointerUp(event);
+        break;
+      case 'touchstart':
+        this._onPointerDown(event.changedTouches[0]);
+        break;
+      case 'touchmove':
+        this._onPointerPan(event.changedTouches[0]);
+        break;
+      case 'touchend':
+        this._onPointerUp(event.changedTouches[0]);
+        break;
+    }
+  }
+
+  setPosition(position) {
+    this.position = position;
+    this.width = this.offsetWidth;
+    this.style.opacity = (this.width - Math.abs(position)) / this.width;
+    const currentY = this.style.transform.split(',')[1];
+    this.style.transform = `translate(${position}px, ${currentY}`;
+  }
+
+  _dismiss() {
+    this.style.opacity = 0;
+
+    const collapseAnim = this.animate(
+        {height: [getComputedStyle(this).height, '0px']},
+        {duration: 100, iterations: 1});
+
+    collapseAnim.onfinish = this._fireRemove.bind(this);
+  }
+
+  _fireRemove() {
+    this.setPosition(0);
+
+    const event = new CustomEvent(
+        'remove', {detail: {itemIndex: this.itemIndex}, bubbles: true});
+    this.dispatchEvent(event);
+  }
+
+  settle(targetPosition) {
+    this.state = 'initial';
+    if (targetPosition === this.position) {
+      return;
+    }
+
+    const currentY = this.style.transform.split(',')[1];
+    const isDismiss = targetPosition !== 0;
+
+    const animation = this.animate(
+        {
+          transform: [
+            `translate(${this.position}px,${currentY}`,
+            `translate(${targetPosition}px,${currentY}`
+          ],
+          opacity: [this.style.opacity, isDismiss ? 0 : 1]
+        },
+        {
+          duration: Math.abs(targetPosition - this.position) * 0.5,
+          iterations: 1
+        });
+
+    this.position = targetPosition;
+    animation.onfinish = () =>
+        isDismiss ? this._dismiss() : this.setPosition(0);
+  }
+
+  fling(velocityX) {
+    this.state = 'initial';
+    const targetPosition = velocityX < 0 ? -this.width : this.width;
+
+    const currentY = this.style.transform.split(',')[1];
+
+    const animation = this.animate(
+        {
+          transform: [
+            `translate(${this.position}px,${currentY}`,
+            `translate(${targetPosition}px,${currentY}`
+          ],
+          opacity: [this.style.opacity, 0]
+        },
+        {
+          duration:
+              Math.abs(targetPosition - this.position) / Math.abs(velocityX),
+          iterations: 1
+        });
+
+    animation.onfinish = this._dismiss.bind(this);
+  }
+
+  _settleToClosestPosition() {
+    const fraction = this.position / this.width;
+    if (fraction > 0.5) {
+      this.settle(this.width);
+    } else if (fraction < -0.5) {
+      this.settle(-this.width);
+    } else {
+      this.settle(0);
+    }
+  }
+
+  _onPointerDown(change) {
+    this.state = 'initial';
+    this.startX = change.clientX;
+    this.startY = change.clientY;
+    this.startPosition = 0;
+  }
+
+  _onPointerPan(change) {
+    if (this.state == 'initial') {
+      const deltaX = change.clientX - this.startX;
+      const deltaY = change.clientY - this.startY;
+
+      // Shaky or sloppy fingers are common using scroll, so
+      // we ignore mini pans of 5 dips like Android.
+      if (deltaX ** 2 + deltaY ** 2 < kTouchSlopValue ** 2) {
+        return;
+      }
+
+      // If scrolled vertically, ignore following events.
+      if (Math.abs(deltaY) > Math.abs(deltaX)) {
+        this.state = 'ignoring';
+        return;
+      }
+
+      this.state = 'dragging';
+    }
+
+    if (this.state == 'dragging') {
+      this._tracker = this._tracker || new VelocityTracker();
+      this._tracker.update(change);
+
+      const deltaX = change.clientX - this.startX;
+      this.setPosition(this.startPosition + deltaX);
+    }
+  }
+
+  _onPointerUp(change) {
+    if (this.state == 'dragging') {
+      const velocity = this._tracker.update(change).velocityX;
+      this._tracker = null;
+      if (Math.abs(velocity) > kMinFlingVelocityValue) {
+        this.fling(velocity);
+      } else {
+        this._settleToClosestPosition();
+      }
+    }
+  }
+}
+
+customElements.define('dismissable-item', DismissableItem);
