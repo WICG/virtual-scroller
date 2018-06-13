44774009456c13d7a17c9870680854e7e5d71a7a
Kenneth Christiansen <kenneth.christiansen@gmail.com>
2018-06-13 08:55:34 +0000

    Fix review comments

--- demo/dismissable/dismissable-item.js
+++ demo/dismissable/dismissable-item.js
@@ -2,43 +2,19 @@ const kMinFlingVelocityValue = 0.4;
 const kTouchSlopValue = 5;
 
 class VelocityTracker {
-  constructor(target) {
-    this.target = target;
-
+  constructor() {
     this._recentTouchMoves = [];
-    this.velocityX = 0;
     this._timeWindow = 50;
-
-    this._onTouchStart = this._onTouchStart.bind(this);
-    this._onTouchMove = this._onTouchMove.bind(this);
-    this._onTouchEnd = this._onTouchEnd.bind(this);
-  }
-
-  start() {
-    this.target.addEventListener('touchstart', this._onTouchStart);
-    this.target.addEventListener('touchmove', this._onTouchMove);
-    this.target.addEventListener('touchend', this._onTouchEnd);
-  }
-
-  stop() {
-    this._recentTouchMoves = [];
-    this.velocityX = 0;
-    this.target.removeEventListener('touchstart', this._onTouchStart);
-    this.target.removeEventListener('touchmove', this._onTouchMove);
-    this.target.removeEventListener('touchend', this._onTouchEnd);
   }
 
   _pruneHistory(timeStamp) {
-    for (let i = 0; i < this._recentTouchMoves.length; ++i) {
-      if (this._recentTouchMoves[i].timeStamp > timeStamp - this._timeWindow) {
-        this._recentTouchMoves = this._recentTouchMoves.slice(i);
-        return;
-      }
-    }
-    this._recentTouchMoves = [];
+    const index = this._recentTouchMoves.findIndex((touch) => {
+      return touch.timeStamp > timeStamp - this._timeWindow;
+    });
+    this._recentTouchMoves.splice(0, index + 1);
   }
 
-  _update(e) {
+  update(e) {
     this._pruneHistory(e.timeStamp);
     this._recentTouchMoves.push(e);
 
@@ -47,25 +23,9 @@ class VelocityTracker {
     const deltaX = e.changedTouches[0].clientX - oldestTouchMove.changedTouches[0].clientX;
     const deltaT = e.timeStamp - oldestTouchMove.timeStamp;
 
-    if (deltaT > 0) {
-      this.velocityX = deltaX / deltaT;
-    } else {
-      this.velocityX = 0;
-    }
-  }
-
-  _onTouchStart(e) {
-    this._recentTouchMoves.push(e);
-    this.velocityX = 0;
-  }
-
-  _onTouchMove(e) {
-    this._update(e);
-  }
-
-  _onTouchEnd(e) {
-    this._update(e);
-    this._recentTouchMoves = [];
+    return {
+      velocityX: (deltaT > 0) ? deltaX / deltaT : 0
+    };
   }
 }
 
@@ -73,23 +33,29 @@ class DismissableItem extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      <div>
-        <slot></slot>
-      </div>
-    `;
-
-    this.tracker = new VelocityTracker(this);
+    this.shadowRoot.innerHTML = '<slot></slot>';
 
     this.position = 0;
     this.itemIndex = 0;
     this.width = 0;
+    this.state = 'initial';
+    this.addEventListener('touchstart', this);
+    this.addEventListener('touchmove', this);
+    this.addEventListener('touchend', this);
+  }
 
-    this.state = "initial";
-
-    this.addEventListener('touchstart', this._onTouchStart.bind(this));
-    this.addEventListener('touchmove', this._onTouchMove.bind(this));
-    this.addEventListener('touchend', this._onTouchEnd.bind(this));
+  handleEvent(event) {
+    switch(event.type) {
+      case 'touchstart':
+        this._onTouchStart(event);
+        break;
+      case 'touchmove':
+        this._onTouchMove(event);
+        break;
+      case 'touchend':
+        this._onTouchEnd(event);
+        break;
+    }
   }
 
   setPosition(position) {
@@ -110,59 +76,51 @@ class DismissableItem extends HTMLElement {
       iterations: 1
     });
 
-    collapseAnim.onfinish = () => {
-      this.setPosition(0);
-      const event = new CustomEvent('remove', { index: this.itemIndex });
-      this.dispatchEvent(event);
-    }
+    collapseAnim.onfinish = this._fireRemove.bind(this);
+  }
+
+  _fireRemove() {
+    this.setPosition(0);
+
+    const event = new CustomEvent('remove', { 
+      detail: { itemIndex: this.itemIndex },
+      bubbles: true
+    });
+    this.dispatchEvent(event);
   }
 
   settle(targetPosition) {
-    this.state = "initial";
-
-    const basePosition = this.position;
-    if (targetPosition === basePosition) {
+    this.state = 'initial';
+    if (targetPosition === this.position) {
       return;
     }
 
-    this.position = targetPosition;
-    const baseOpacity = this.style.opacity;
-
     const currentY = this.style.transform.split(',')[1];
-
     const isDismiss = targetPosition !== 0;
 
     const animation = this.animate([
-      { transform: `translate(${basePosition}px,${currentY}`, opacity: baseOpacity },
+      { transform: `translate(${this.position}px,${currentY}`, opacity: this.style.opacity },
       { transform: `translate(${targetPosition}px,${currentY}`, opacity: isDismiss ? 0 : 1}
     ], { 
-      duration: Math.abs(targetPosition - basePosition) * 0.5,
+      duration: Math.abs(targetPosition - this.position) * 0.5,
       iterations: 1
     });
 
-    animation.onfinish = () => {
-      if (isDismiss) {
-        this._dismiss();
-      } else {
-        this.setPosition(0);
-      }
-    }
+    this.position = targetPosition;
+    animation.onfinish = () => isDismiss ? this._dismiss() : this.setPosition(0);
   }
 
   fling(velocityX) {
-    this.state = "initial";
-    const basePosition = this.position;
+    this.state = 'initial';
     const targetPosition = velocityX < 0 ? -this.width : this.width;
-
-    const baseOpacity = this.style.opacity;
 
     const currentY = this.style.transform.split(',')[1];
 
     const animation = this.animate([
-      { transform: `translate(${basePosition}px,${currentY}`, opacity: baseOpacity },
+      { transform: `translate(${this.position}px,${currentY}`, opacity: this.style.opacity },
       { transform: `translate(${targetPosition}px,${currentY}`, opacity: 0 }
     ], { 
-      duration: Math.abs(targetPosition - basePosition) / Math.abs(velocityX),
+      duration: Math.abs(targetPosition - this.position) / Math.abs(velocityX),
       iterations: 1
     });
 
@@ -181,15 +139,14 @@ class DismissableItem extends HTMLElement {
   } 
 
   _onTouchStart(e) { 
-    this.state = "initial";
-
+    this.state = 'initial';
     this.startX = e.changedTouches[0].clientX;
     this.startY = e.changedTouches[0].clientY;
     this.startPosition = 0;
   }
 
   _onTouchMove(e) { 
-    if (this.state == "initial") {
+    if (this.state == 'initial') {
       const deltaX = e.changedTouches[0].clientX - this.startX;
       const deltaY = e.changedTouches[0].clientY - this.startY;
 
@@ -203,12 +160,14 @@ class DismissableItem extends HTMLElement {
         return;
       }
 
-      this.state = "dragging";
+      this.state = 'dragging';
       this.width = this.offsetWidth;
-      this.tracker.start();
     }
 
-    if (this.state == "dragging") {
+    if (this.state == 'dragging') {
+      this._tracker = this._tracker || new VelocityTracker();
+      this._tracker.update(e);
+
       e.preventDefault();
       const deltaX = e.changedTouches[0].clientX - this.startX;
       this.position = this.startPosition + deltaX;
@@ -217,10 +176,10 @@ class DismissableItem extends HTMLElement {
   }
 
   _onTouchEnd(e) { 
-    this.tracker.stop();
+    const velocity = this._tracker.update(e).velocityX;
+    this._tracker = null;
 
-    if (this.state == "dragging") {
-      const velocity = this.tracker.velocityX;
+    if (this.state == 'dragging') {
       if (Math.abs(velocity) > kMinFlingVelocityValue) {
         this.fling(velocity);
         return;
