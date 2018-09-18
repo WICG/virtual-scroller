bce2c50af43751a7088647e0dd18695646f38081
Russell Bicknell <bicknellr@google.com>
2018-09-18 11:31:00 -0700

    HeightEstimator: use symbols for properties; keep track of total and count manually so that the average can be computed without making / reducing an array.

--- experiments/scroller-IntersectionObserver/HeightEstimator.js
+++ experiments/scroller-IntersectionObserver/HeightEstimator.js
@@ -1,22 +1,34 @@
+const _known = Symbol('HeightEstimator#[_known]');
+const _total = Symbol('HeightEstimator#[_total]');
+const _count = Symbol('HeightEstimator#[_count]');
+
 export class HeightEstimator {
   constructor() {
-    this._known = new Map();
+    this[_known] = new Map();
+    this[_total] = 0;
+    this[_count] = 0;
   }
 
   set(item, height) {
-    this._known.set(item, height);
+    this[_known].set(item, height);
+    this[_total] += height;
+    this[_count] += 1;
   }
 
   delete(item) {
-    this._known.delete(item);
+    if (this[_known].has(item)) {
+      const height = this[_known].get(item);
+      this[_known].delete(item);
+      this[_total] -= height;
+      this[_count] -= 1;
+    }
   }
 
   estimateHeight(item) {
-    if (this._known.has(item)) {
-      return this._known.get(item);
+    if (this[_known].has(item)) {
+      return this[_known].get(item);
     }
 
-    const known = Array.from(this._known.values());
-    return known.reduce((a, b) => a + b, 0) / known.length;
+    return this[_total] / this[_count];
   }
 };
