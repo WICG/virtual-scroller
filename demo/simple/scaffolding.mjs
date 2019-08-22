228b5b9402b3f49d6191a50089e01b2c1ee574f5
Fergal Daly <fergal@esatclear.ie>
2019-08-22 10:01:59 +0900

    factor out scaffolding.mjs

---
+++ demo/simple/scaffolding.mjs
@@ -0,0 +1,7 @@
+// Conveniently import all of the demo stuff.
+import '../util/confirm-features.mjs';
+import '../util/fps-display.mjs';
+import '../util/demo-controller.mjs';
+// Import this dynamically. If virtual-scroller is not available, the
+// demo will still work.
+import('std:elements/virtual-scroller');
